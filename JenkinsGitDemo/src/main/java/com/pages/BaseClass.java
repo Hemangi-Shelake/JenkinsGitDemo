d2f8ed1a2639bae8ad64c957be200d613df8d9cf
ambin03202 <ambin03202@192.168.1.203>
2020-10-04 14:48:45 +0530

    First Commit

---
+++ JenkinsGitDemo/src/main/java/com/pages/BaseClass.java
@@ -0,0 +1,41 @@
+package com.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	
+	WebDriver driver;
+	
+	
+	@BeforeMethod
+	public void setUpMethod()
+	{
+		if(System.getProperty("browser").equalsIgnoreCase("Chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+		}
+		else if(System.getProperty("browser").equalsIgnoreCase("Firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+		}
+
+		driver.manage().window().maximize();
+		driver.get(System.getProperty("url"));
+		driver.navigate().to("https://google.com");
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.close();
+	}
+
+}
---
+++ JenkinsGitDemo/src/test/java/com/test/TestCases.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+import com.pages.BaseClass;
+
+public class TestCases extends BaseClass{
+	
+	@Test
+	public void testCase1()
+	{
+		System.out.println("Test Case 1");
+	}
+	
+	@Test
+	public void testCase2()
+	{
+		System.out.println("Test Case 2");
+	}
+	
+
+	
+
+}
